5ec98f62726e42ae85d29d13254554eb0ac45c93
Konrad Pelc <kpelc54@gmail.com>
2024-05-20 21:25:28 +0200

    Init

---
+++ src/main/java/com/example/MikoEvents/MikoEventsApplication.java
@@ -0,0 +1,13 @@
+package com.example.MikoEvents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MikoEventsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MikoEventsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MikoEvents/MikoEventsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MikoEvents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MikoEventsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
